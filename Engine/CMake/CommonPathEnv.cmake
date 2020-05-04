a5a4b2b49c34e8efda3d585b45ca7fdb189e58a8
BritzSTM <ha40672@gmail.com>
2020-05-04 11:48:19 +0900

    Create CommonPathEnv.cmake

---
+++ Engine/CMake/CommonPathEnv.cmake
@@ -0,0 +1,10 @@
+﻿#공통 환경변수를 정의하기 위한 모듈파일
+cmake_minimum_required(VERSION 3.7)
+
+
+#name section
+set(LAB_INSTALLED_DIR_NAME Installed)
+
+
+#path section
+set(LAB_INSTALLED_PATH "${CMAKE_CURRENT_LIST_DIR}/../${LAB_INSTALLED_DIR_NAME}")
