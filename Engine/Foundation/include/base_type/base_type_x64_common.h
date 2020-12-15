f141618af17831127d8a531e83f6306129a6dff6
BritzSTM <ha40672@gmail.com>
2020-12-15 22:44:08 +0900

    Fix x64 common type

--- Engine/Foundation/include/base_type/base_type_x64_common.h
+++ Engine/Foundation/include/base_type/base_type_x64_common.h
@@ -20,8 +20,8 @@ namespace fd
         using int64 = std::int64_t;
 
         using uint8 = std::uint8_t;
-        using uint16 = std::int16_t;
-        using uint32 = std::int32_t;
-        using uint64 = std::int64_t;
+        using uint16 = std::uint16_t;
+        using uint32 = std::uint32_t;
+        using uint64 = std::uint64_t;
     }
 }
