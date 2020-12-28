38fb2c96d4c109682d26977a5f7c39d02d147767
BritzSTM <ha40672@gmail.com>
2020-12-29 03:39:34 +0900

    Rewrite type_name
    vs ide의 front end compiler와 호환성 문제가 발생하여 새로운 구조로 다시 작성하였음.

--- Engine/Foundation/include/reflection/type_name.h
+++ Engine/Foundation/include/reflection/type_name.h
@@ -13,6 +13,30 @@
 #pragma once
 
 #include "../compiler_directive.h"
+#include "../base_types.h"
+
+#include <array>
+#include <string_view>
+
+
+namespace fd::refl
+{
+    /*
+        이 유형을 컴파일러 특성별(_CompilerTag)로 특수화 할 것. 
+        아래 정의된 데이터 맴버는 반드시 제공되어야 함.
+    */
+    template<typename _Ty, typename _CompilerTag = CurrCompilerTag>
+    struct base_type_name_traits
+    {
+        static_assert(false, "Unknown compiler tag.");
+
+        static constexpr std::string_view rawName{ nullptr };
+        static constexpr std::string_view fixedPrefix{ nullptr };
+        static constexpr std::string_view fixedSubfix{ nullptr };
+
+        static constexpr size_t kDynamicPrefixLen{ 0 }; /**< 고정길이 접두사 이후 동적인 접두사의 길이 */
+    };
+}
 
 
 #if (FD_CURR_COMPILER == FD_MSVC_COMPILER)
@@ -26,11 +50,86 @@
 
 #endif
 
+
 namespace fd::refl
 {
+    namespace _internal_type_name
+    {
+        template<typename _Ty>
+        inline static constexpr size_t GetRawTypeNameStartPos() noexcept
+        {
+            using bt = base_type_name_traits<_Ty>;
+            return bt::fixedPrefix.size() + bt::kDynamicPrefixLen;
+        }
+
+        template<typename _Ty>
+        inline static constexpr size_t GetRawTypeNameEndPos() noexcept
+        {
+            using bt = base_type_name_traits<_Ty>;
+            return bt::rawName.size() - bt::fixedSubfix.size();
+        }
+
+        template<typename _Ty>
+        constexpr size_t CalNormalizedFullNameLen() noexcept
+        {
+            using bt = base_type_name_traits<_Ty>;
+
+            size_t len{ 0 };
+            for (size_t i{ GetRawTypeNameStartPos<_Ty>() }; i < GetRawTypeNameEndPos<_Ty>(); ++i)
+            {
+                if (bt::rawName[i] != ' ')
+                {
+                    ++len;
+                }
+            }
+
+            return len;
+        }
+
+        template<typename _Ty>
+        constexpr auto GetNormalizedFullName() noexcept
+        {
+            using namespace std;
+            using bt = base_type_name_traits<_Ty>;
+
+            constexpr size_t kArrSize{ CalNormalizedFullNameLen<_Ty>() };
+            array<char, kArrSize + 1> arr{ };
+
+            for (size_t tyIdx{ GetRawTypeNameStartPos<_Ty>() }, arrIdx{ 0 }; tyIdx < GetRawTypeNameEndPos<_Ty>(); ++tyIdx)
+            {
+                if (bt::rawName[tyIdx] != ' ')
+                {
+                    arr[arrIdx++] = bt::rawName[tyIdx];
+                }
+            }
+            arr[kArrSize] = '\0';
+
+            return arr;
+        }
+    }
+
     template<typename _Ty>
     struct type_name_traits
     {
+        static constexpr auto normalizedFullName{ _internal_type_name::GetNormalizedFullName<_Ty>() }; /** Normalized name in std::array */
 
+        /** 해당유형의 이름과 비교합니다. 컴파일 타임에 이름 비교시 이 함수를 이용할 것. */
+        static constexpr bool compare(std::string_view name) noexcept
+        {
+            if ((normalizedFullName.size() - 1) != name.size())
+            {
+                return false;
+            }
+
+            for (size_t i{ 0 }; i < name.size(); ++i)
+            {
+                if (name[i] != normalizedFullName[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     };
 }
--- Engine/Foundation/include/reflection/type_name_msvc.h
+++ Engine/Foundation/include/reflection/type_name_msvc.h
@@ -1,71 +1,66 @@
+/*
+    ! 이 헤더파일을 직접 포함하지 말 것. 
+*/
 #pragma once
-
-#include <string_view>
-#include <array>
 
 
 namespace fd::refl
 {
+    /*
+        @brief 타입이름이 포함된 문자열을 획득합니다. 라이브러리 사용자는 이 함수를 직접 호출하지 말 것.
+        @detail
+         이 함수는 정의는 규칙을 위반하면서 정의했습니다. MSVC의 __FUNCSIG__ 매크로는 해당함수의 서명을 문자열로 획득하기 때문입니다.
+        만약 어떤 namespace에 이 함수가 정의된다면 namespace가 포함된 더 길다란 문자열이 생성됩니다.
+    */
+    template<typename _Ty>
+    constexpr auto __cdecl _rn_vs(int = 0) noexcept
+    {
+        return __FUNCSIG__;
+    }
+
     namespace _internal_type_name_msvc
     {
-        /*
-            컴파일 완료시 __FUNCSIG__는 다음과 같은 형식을 지닌다.
-
-            class std::basic_string_view<char, std::char_traits<char>> __cdecl GetFullName<enum fd::ABType>(void) noexcept
-            class std::basic_string_view<char, std::char_traits<char>> __cdecl GetFullName<class std::basic_string_view<char, std::char_traits<char>>>(void) noexcept
-
-            컴파일 이전이라면 intellisense엔진은 enum같은 형식을 생략하고 함수 전달인자 void를 생략
-        */
-        constexpr std::string_view ExtractTypeFullNameImpl(std::string_view fnSig) noexcept
+        constexpr size_t GetDynamicPrefixLen(std::string_view rawName, std::string_view fixedPrefix) noexcept
         {
             using namespace std;
 
-            if (size_t leftPos{ fnSig.find("ExtractTypeFullName<") }; leftPos != fnSig.npos)
-            {
-                fnSig.remove_prefix(leftPos + 20);
+            // 공백이 포함된 동적 접두사들
+            constexpr array<string_view, 4> dyPrefixs{ "enum ", "class ", "struct ", "union " };
 
-                // msvc는 컴파일 후 일부 유형이 포함된다. 컴파일러별 공통적으로 포맷된 문자열 제공을 위해 제거한다
-                constexpr array<const char*, 4> pszTypes{ "enum ", "class ", "struct ", "union " };
-                for (auto i{ 0 }; i < pszTypes.size(); ++i)
+            size_t dyLen{ 0 };
+            for (const auto& dy : dyPrefixs)
+            {
+                bool found{ true };
+
+                //fixedPrefix 이후에 dy를 찾는다.
+                for (size_t dyIdx{ 0 }, rawIdx{ fixedPrefix.size() };
+                    dyIdx < dy.size(); ++dyIdx, ++rawIdx)
                 {
-                    if (fnSig.find(pszTypes[i]) != fnSig.npos)
+                    if (dy[dyIdx] != rawName[rawIdx])
                     {
-                        fnSig.remove_prefix(char_traits<char>::length(pszTypes[i]));
+                        found = false;
                         break;
                     }
+                }            
+
+                if (found)
+                {
+                    dyLen = dy.size();
+                    break;
                 }
             }
-            else
-            {
-                return {};
-            }
 
-            if (size_t rightPos{ fnSig.rfind(">() noexcept") }; rightPos != fnSig.npos)
-            {
-                fnSig.remove_suffix(12);
-            }
-            else if (size_t rightPos{ fnSig.rfind(">(void) noexcept") }; rightPos != fnSig.npos)
-            {
-                fnSig.remove_suffix(16);
-            }
-            else
-            {
-                return {};
-            }
-
-            return fnSig;
-        }
-
-        template<typename _Ty>
-        constexpr std::string_view ExtractTypeFullName() noexcept
-        {
-            return ExtractTypeFullNameImpl(__FUNCSIG__);
+            return dyLen;
         }
     }
 
     template<typename _Ty>
-    constexpr std::string_view GetTypeFullName() noexcept
+    struct base_type_name_traits<_Ty, MsvcCompilerTag>
     {
-        return _internal_type_name_msvc::ExtractTypeFullName<_Ty>();
-    }
+        static constexpr std::string_view rawName{ _rn_vs<_Ty>() };
+        static constexpr std::string_view fixedPrefix{ "auto __cdecl fd::refl::_rn_vs<" };
+        static constexpr std::string_view fixedSubfix{ ">(int) noexcept" };
+
+        static constexpr size_t kDynamicPrefixLen{ _internal_type_name_msvc::GetDynamicPrefixLen(rawName, fixedPrefix) };
+    };
 }
